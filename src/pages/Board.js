6a42ca9d3550df42bd21e9681996b887055dea6c
Marco Zampini <marco@bloom-it.io>
2022-04-18 14:53:44 +0200

    boards/boardId - Add single board page

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,60 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
+const API_URL = process.env.REACT_APP_API_URL
 
 const Board = () => {
+  const savedToken = localStorage.getItem('authToken')
+
+  const [board, setBoard] = useState(null)
+  const { boardId } = useParams()
+
+  const getBoard = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/boards/${boardId}`, {
+        headers: { Authorization: `Bearer ${savedToken}` },
+      })
+      setBoard(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getBoard()
+  }, [])
+
   return (
     <>
-      <h1>Names for my grandson</h1>
-
-      <p>Created by me - I'm the owner</p>
-      <p>3 members</p>
-      <p>42 names</p>
-
-      <p>There is a list for every participant</p>
-      <p>The user can edit only the list that owns</p>
-      <div>
-        <div>
-          <h2>My list</h2>
-          <ul>
-            <li>Aida</li>
-            <li>Alma</li>
-            <li>Anna</li>
-            <li>Anita</li>
-            <li>Akanksha</li>
-          </ul>
-          <a href="/wireframe/groups-id-edit.html">Edit mode</a>
-        </div>
-        <div>
-          <h2>Anna's list</h2>
-          <ul>
-            <li>Aida</li>
-            <li>Alma</li>
-            <li>Anna</li>
-            <li>Anita</li>
-            <li>Akanksha</li>
-          </ul>
-        </div>
-        <div>
-          <h2>Brian's list</h2>
-          <ul>
-            <li>Aida</li>
-            <li>Alma</li>
-            <li>Anna</li>
-            <li>Anita</li>
-            <li>Akanksha</li>
-          </ul>
-        </div>
-        <div>
-          <h2>Akanksha's list</h2>
-          <ul>
-            <li>Aida</li>
-            <li>Alma</li>
-            <li>Anna</li>
-            <li>Anita</li>
-            <li>Akanksha</li>
-          </ul>
-        </div>
-      </div>
+      {board && (
+        <>
+          <h1>Board {board.name}</h1>
+          <p>Created by {board.owner}</p>
+          {board.lists.map((list) => {
+            return (
+              <div key={list._id}>
+                <h2>List by {list.owner}</h2>
+                <p>List id: {list._id}</p>
+                <p>Status: {list.status}</p>
+                <ul>
+                  {list.names.map((name) => {
+                    return <li key={name._id}>{name.value}</li>
+                  })}
+                </ul>
+              </div>
+            )
+          })}
+        </>
+      )}
     </>
   )
 }

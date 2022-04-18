e35fa128650fd1a43d2c0b111c2de0ad3485195c
Marco Zampini <marco@bloom-it.io>
2022-04-18 18:08:52 +0200

    boards/style - Add basic style for boards and lists

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
+import './Boards.css'
+
 const API_URL = process.env.REACT_APP_API_URL
 
 const Board = () => {
@@ -30,24 +32,26 @@ const Board = () => {
         <>
           <h1>Board {board.name}</h1>
           <p>Created by {board.owner}</p>
-          {board.lists.map((list) => {
-            return (
-              <div key={list._id}>
-                <h2>List by {list.owner}</h2>
-                <p>List id: {list._id}</p>
-                <p>Status: {list.status}</p>
-                <ul>
-                  {list.names.map((name) => {
-                    return (
-                      <li key={name._id}>
-                        {name.value} - w: {name.weight}
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )
-          })}
+          <div className="lists">
+            {board.lists.map((list) => {
+              return (
+                <div className="list" key={list._id}>
+                  <h2>List by {list.owner}</h2>
+                  <p>List id: {list._id}</p>
+                  <p>Status: {list.status}</p>
+                  <ul>
+                    {list.names.map((name) => {
+                      return (
+                        <li key={name._id}>
+                          {name.value} - w: {name.weight}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
         </>
       )}
     </>
--- src/pages/Boards.js
+++ src/pages/Boards.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import NewBoardForm from '../components/NewBoardForm'
+import './Boards.css'
+
 const API_URL = process.env.REACT_APP_API_URL
 
 const Boards = () => {

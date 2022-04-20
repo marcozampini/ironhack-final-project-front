ab796f1f6c91cbf27d0b3f71cf794b369d548d6a
bvalette <bvalette@student.42.fr>
2022-04-20 12:36:27 +0200

    componnent/refractor - nameSearch and Name

--- src/components/names/Name.js
+++ src/components/names/Name.js
@@ -1,35 +1,50 @@
 import { httpStatus } from 'http-status'
 import { useContext, useState } from 'react'
 import { BoardContext } from '../../context/board.context'
+import { CurrentBoardContext } from '../../context/currentBoard.context'
 
 const Name = ({ data, list }) => {
+  const { currentBoard, currentBoardOwnedList, fetchBoard } =
+    useContext(CurrentBoardContext)
   const [isInList, setIsInList] = useState(
-    list?.names.some((item) => item.value === data.name.value)
+    currentBoardOwnedList?.names.some((item) => item.value === data.name.value)
   )
   const { addName, deleteName } = useContext(BoardContext)
 
   async function handleAdd() {
     const status = await addName(list._id, data.name, 42)
     if (status === 200) {
-      setIsInList(true)
+      console.log('adding and fetching data for ', currentBoard._id)
+      await fetchBoard(currentBoard._id)
     }
+    setIsInList(
+      currentBoardOwnedList?.names.some(
+        (item) => item.value === data.name.value
+      )
+    )
   }
 
   async function handleDelete() {
     const status = await deleteName(list._id, data.name._id)
     if (status === 204) {
-      setIsInList(false)
+      console.log('removing and fetching data for ', currentBoard._id)
+      await fetchBoard(currentBoard._id)
     }
+    setIsInList(
+      currentBoardOwnedList?.names.some(
+        (item) => item.value === data.name.value
+      )
+    )
   }
+
+
 
   return (
     <>
       <div>
         {data.name.value}
         {isInList ? (
-          <button onClick={handleDelete}>
-            REMOVE
-          </button>
+          <button onClick={handleDelete}>REMOVE</button>
         ) : (
           // TODO - remove 42 test only
           <button onClick={handleAdd}>ADD</button>

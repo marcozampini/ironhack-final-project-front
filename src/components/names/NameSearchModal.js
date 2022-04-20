f53f0334678e8f4516710295867bc27a227af7fb
bvalette <bvalette@student.42.fr>
2022-04-20 09:38:08 +0200

    component/add - name search and name

---
+++ src/components/names/NameSearchModal.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { NameSearchContext } from '../../context/nameSearch.context';
+import Name from '../names/Name';
+import './NameSearchModal.css';
+
+const NameSearchModal = ({ isVisible, toggleVisibility }) => {
+  const {
+    formData,
+    handleChanges,
+    handleSubmit,
+    searchResults,
+    isRequestSent,
+    currentBoard,
+  } = useContext(NameSearchContext);
+
+  return (
+    <>
+      {isVisible && (
+        <div className='modal-container'>
+          <div className='modal-content'>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='q'>Name</label>
+              <input
+                id='q'
+                type='text'
+                value={formData.q}
+                onChange={handleChanges}
+              />
+              <input type='submit' value='search' />
+            </form>
+            <button className='modalCloseButton' onClick={toggleVisibility}>
+              CLOSE
+            </button>
+            <div className='searchResults'>
+              {(searchResults.length &&
+                searchResults
+                  .sort((a, b) => a.name.value.length - b.name.value.length)
+                  .map((item) => (
+                    <Name
+                      key={item._id}
+                      data={item}
+                      list={currentBoard?.lists.find((l) => l.isOwner)}
+                    />
+                  ))) ||
+                (isRequestSent && 'No results...')}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NameSearchModal;
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,58 +1,65 @@
-import { useState, useEffect, useCallback } from 'react'
-import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
-import './Boards.css'
-import NameSearchModal from '../components/layout/NameSearchModal'
+import { useState, useEffect, useCallback, useContext } from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import './Boards.css';
+import NameSearchModal from '../components/names/NameSearchModal';
+import { NameSearchContext } from '../context/nameSearch.context';
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Board = () => {
-  const savedToken = localStorage.getItem('authToken')
-  const navigate = useNavigate()
-  const [board, setBoard] = useState(null)
-  const [modalVisible, setModalVisible] = useState(false)
-  const { boardId } = useParams()
+  const savedToken = localStorage.getItem('authToken');
+  const navigate = useNavigate();
+  const [board, setBoard] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const { boardId } = useParams();
+  const { setCurrentBoard } = useContext(NameSearchContext);
 
   const getBoard = useCallback(async () => {
     try {
       const response = await axios.get(`${API_URL}/boards/${boardId}`, {
         headers: { Authorization: `Bearer ${savedToken}` },
-      })
-      setBoard(response.data)
+      });
+      setBoard(response.data);
+      // set nameSearchContext currentBoard so adding name from the search result is possible
+      setCurrentBoard(response.data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }, [boardId, savedToken])
+  }, [boardId, savedToken, setCurrentBoard]);
 
   useEffect(() => {
-    getBoard()
-  }, [getBoard])
+    getBoard();
+  }, [getBoard]);
 
   const handleDelete = async (event) => {
-    const confirmed = window.confirm('Do you want to delete this board?')
+    const confirmed = window.confirm('Do you want to delete this board?');
     if (confirmed) {
       try {
         await axios.delete(`${API_URL}/boards/${boardId}`, {
           headers: { Authorization: `Bearer ${savedToken}` },
-        })
-        navigate('/boards')
+        });
+        navigate('/boards');
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
-  }
+  };
 
   const toggleModalVisibility = () => {
     setModalVisible(!modalVisible);
-  }
+  };
 
   const handleAddName = async () => {
     toggleModalVisibility();
-  }
+  };
 
   return (
     <>
-      <NameSearchModal boardId={boardId} isVisible={modalVisible} toggleVisibility={toggleModalVisibility} />
+      <NameSearchModal
+        isVisible={modalVisible}
+        toggleVisibility={toggleModalVisibility}
+      />
       {board && (
         <>
           <h1>Board {board.name}</h1>
@@ -61,30 +68,30 @@ const Board = () => {
           {board.isOwner && (
             <button onClick={handleDelete}>Delete board</button>
           )}
-          <div className="lists">
+          <div className='lists'>
             {board.lists.map((list) => {
               return (
-                <div className="list" key={list._id}>
+                <div className='list' key={list._id}>
                   <h2>List by {list.owner}</h2>
                   {list.isOwner && <span>It's me!</span>}
                   <p>List id: {list._id}</p>
                   <ul>
                     {list.names.map((name) => {
                       return (
-                        <li key={name._id}>
+                        <li key={list._id + '--' + name._id}>
                           {name.value} - w: {name.weight}
                         </li>
-                      )
+                      );
                     })}
                   </ul>
                 </div>
-              )
+              );
             })}
           </div>
         </>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;

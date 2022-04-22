4e69973044bee71da18500e6457a5f06240cc71e
Marco Zampini <marco@bloom-it.io>
2022-04-22 03:38:29 +0200

    404/style Add style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
   return (
     <>
-      <h1>Sorry... this pages leads nowhere</h1>
-      <p>
-        {' '}
-        <Link to="/"> Let's get you back home</Link>
-      </p>
+      <h1>Sorry...</h1>
+      <div className="main-content">
+        <p>...this page leads nowhere.</p>
+        <p>
+          <Link to="/"> Let's get you back home!</Link>
+        </p>
+      </div>
     </>
   )
 }

08b040b8c24a6a085c523c879391a204de0a4894
Marco Zampini <marco@bloom-it.io>
2022-04-22 06:15:00 +0200

    board/add Add style

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -81,11 +81,18 @@ const Board = () => {
               textAfter={"'s board"}
             />
           </h1>
+
           {currentBoard.isOwner ? (
-            <>
-              <button onClick={toggleAddFormVisibility}>Add participant</button>
-              <button onClick={toggleRenameFormVisibility}>Rename board</button>
-              <button onClick={handleDeleteBoard}>Delete board</button>
+            <div className="main-content">
+              <button onClick={toggleAddFormVisibility}>
+                <i className="fa-solid fa-user-plus"></i> Add participant
+              </button>{' '}
+              <button onClick={toggleRenameFormVisibility}>
+                <i class="fa-solid fa-pencil"></i> Rename board
+              </button>{' '}
+              <button onClick={handleDeleteBoard}>
+                <i className="fa-solid fa-trash-can"></i> Delete board
+              </button>
               <NewParticipantForm
                 isVisible={addFormVisible}
                 toggleVisibility={toggleAddFormVisibility}
@@ -95,7 +102,7 @@ const Board = () => {
                 isVisible={renameFormVisible}
                 toggleVisibility={toggleRenameFormVisibility}
               />
-            </>
+            </div>
           ) : (
             <></>
           )}
